bd4ce633761f99d396f1ff0f103476d1d3fd1f52
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2023-09-25 12:00:59 +1000

    Completed week3 lab

--- Week01/my-app/App.js
+++ Week01/my-app/App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Switch, Text, View } from 'react-native';
 
 export default function App() {
+  const toggleSwitch = () => {
+    
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textHeading}>Hello World</Text>
+      <Text style={styles.whiteText}>It is a pleasure to be here!</Text>
+      {/* <StatusBar style="auto" /> We don't need this */}
+      <Image alt="Some random text" 
+      style={styles.image} 
+      source={{uri:'https://picsum.photos/200/200'}}
+      />
+
+      <Button style={styles.button} 
+      title="Click me!" 
+      onPress={() => {alert("Hello Hooman!")}}
+      color={"#de218b"}
+      />
+
+      <Switch />
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textHeading: {
+    color: '#de218b',
+    fontSize: 50,
+    fontWeight: 'bold',
+    textDecorationLine: "underline",
+  },
+  whiteText: {
+    color: 'white',
+    fontSize: 30,
+  }, 
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 50,
+  },
+  button: {
+    padding: 100,
+    height: 100,
+    width: 100
+  }
 });
---
+++ Week03/Week03Lab/lab03/App.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+
+export default function App() {
+  const [isImageVisible, setIsImageVisible] = useState(false);
+
+  const triggerImage = () => {
+    setIsImageVisible(!isImageVisible);
+  };
+
+  return (
+    <View style={styles.masterContainer}>
+      <Text style={styles.heading}>Hello, I'm Arindam!</Text>
+      <Text style={styles.normalText}>Here are five facts about me:</Text>
+      <Text style={styles.listElement}>
+        1. I am currently in my second year, almost reaching my third :(
+      </Text>
+      <Text style={styles.listElement}>2. I am a level 100 procrastinator</Text>
+      <Text style={styles.listElement}>3. I am 19 years old</Text>
+      <Text style={styles.listElement}>
+        4. I love video games, I think I have played all AAA games
+      </Text>
+      <Text style={styles.listElement}>
+        5. I need to touch grass more often
+      </Text>
+      <Text style={styles.normalText}>Here is a cool image!</Text>
+      {isImageVisible && (
+        <Image
+          source={{ uri: "https://picsum.photos/200/200" }}
+          style={styles.image}
+        />
+      )}
+      <Button
+        title={
+          isImageVisible
+            ? "Click me to close me!"
+            : "Click me for a surprise :)"
+        }
+        color={"#de218b"}
+        onPress={triggerImage}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  masterContainer: {
+    flex: 1,
+    backgroundColor: "#121212",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    color: "#de218b",
+    fontSize: 40,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  normalText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  listElement: {
+    color: "white",
+    fontSize: 15,
+    marginBottom: 12,
+    alignSelf: "flex-start",
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
+});
